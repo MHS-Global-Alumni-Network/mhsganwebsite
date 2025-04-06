e3978f19be7e8c755b65c8b1e267afffea350ac5
jandarli <janicedarling1@gmail.com>
2025-04-06 19:02:47 -0400

    feat: Starting work on template pages

--- mhsganwebsite/mhsganwebsite/urls.py
+++ mhsganwebsite/mhsganwebsite/urls.py
@@ -16,7 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.views.generic.base import TemplateView
+
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name="index"),
+    path('about', views.about, name="about"),
+    path('contact', views.contact, name="contact"),
+    path('events', views.events, name="events"),
 ]
