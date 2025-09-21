c22b043fe2554baf308156b40b567ffdf5d06e59
jandarli <janicedarling1@gmail.com>
2025-09-21 18:04:33 -0400

    feat: Adding events app

--- mhsganwebsite/mhsganwebsite/settings.py
+++ mhsganwebsite/mhsganwebsite/settings.py
@@ -45,7 +45,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chapters'
+    'django_extensions',
+    'chapters',
+    'events'
 ]
 
 MIDDLEWARE = [
